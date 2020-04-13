3c7b471f0303709f186fc4a05ca3b99c56d4b891
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-04-13 10:18:37 +0530

    Feature of computation of luckiest and unluckiest day added and code refactored

--- gambling.sh
+++ gambling.sh
@@ -7,6 +7,10 @@ daysLost=0;
 declare everyDayGameStatus;
 declare winingDays;
 declare losingDays;
+declare everyDaysWonnings;
+declare everyDaysLosses;
+luckiestDay=1;
+unLuckiestDay=1
 
 for (( counter=1; counter<=20; counter++))
 do
@@ -14,17 +18,23 @@ stack=$(( $stack + 100));
 max=$(( $stack + $(( $stack / 2)) ));
 min=$(( $stack - $(( $stack / 2)) ));
 temp=$stack;
+toDayWinnings=0;
+toDayLosses=0;
     while (( $stack < $max && $stack > $min))
     do
         gameStatus=$(( RANDOM % 2));
         if (( gameStatus == 0)); then
             echo "you win";
-            stack=$(( $stack + 1));
+            stack=$(( $stack + $betAmount));
+            toDayWinnings=$(( $toDayWinnings + 1 ));
         else
             echo "you lost";
-            stack=$(( $stack - 1));
+            stack=$(( $stack - $betAmount));
+            toDayLosses=$(( $toDayLosses + 1 ));
         fi
     done
+    everyDaysWonnings[$counter]=$toDayWinnings;
+    everyDaysLosses[$counter]=$toDayLosses;
     everyDayGameStatus[$counter]=$stack;
     if (( $stack > $temp));then
         daysWon=$(( $daysWon + 1));
@@ -34,7 +44,20 @@ temp=$stack;
         losingDays[$counter]=$(( $temp - $stack));
     fi
 done
+
+for (( counter=2; counter<=20; counter++))
+do
+    if (( ${everyDaysWonnings[$luckiestDay]} < ${everyDaysWonnings[$counter]} )) ; then
+        luckiestDay=$counter;
+    fi
+    if (( ${everyDaysLosses[$unLuckiestDay]} < ${everyDaysLosses[$counter]} )); then
+        unLuckiestDay=$counter;
+    fi
+done
+
 echo ${everyDayGameStatus[@]};
 echo $daysWon;
 echo $daysLost;
+echo $luckiestDay;
+echo $unLuckiestDay
 echo $stack;
