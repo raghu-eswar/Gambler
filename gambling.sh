0b7ce792f0fb6ad4577ddf078594117b31e4e423
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-04-13 09:44:54 +0530

    Feature added to store every day game status

--- gambling.sh
+++ gambling.sh
@@ -1,19 +1,26 @@
 echo "welcome";
 
-stack=100;
+stack=0;
 betAmount=1;
+declare everyDayGameStatus;
+
+for (( counter=1; counter<=20; counter++))
+do
+stack=$(($stack + 100));
 max=$(( $stack + $(( $stack / 2)) ));
 min=$(( $stack - $(( $stack / 2)) ));
-
-while (( $stack < $max && $stack > $min))
-do
-    gameStatus=$(( RANDOM % 2));
-    if (( gameStatus == 0)); then
-        echo "you win";
-        stack=$(( $stack + 1));
-    else
-        echo "you lost";
-        stack=$(( $stack - 1));
-    fi
+    while (( $stack < $max && $stack > $min))
+    do
+        gameStatus=$(( RANDOM % 2));
+        if (( gameStatus == 0)); then
+            echo "you win";
+            stack=$(( $stack + 1));
+        else
+            echo "you lost";
+            stack=$(( $stack - 1));
+        fi
+    done
+    everyDayGameStatus[$counter]=$stack;
 done
+echo ${everyDayGameStatus[@]};
 echo $stack;
