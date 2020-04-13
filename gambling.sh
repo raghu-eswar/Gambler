4ce22716dbe82bac47296a413e78712342763d15
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-04-13 10:01:56 +0530

    Feature added to store days own and lost

--- gambling.sh
+++ gambling.sh
@@ -2,13 +2,18 @@ echo "welcome";
 
 stack=0;
 betAmount=1;
+daysWon=0;
+daysLost=0;
 declare everyDayGameStatus;
+declare winingDays;
+declare losingDays;
 
 for (( counter=1; counter<=20; counter++))
 do
-stack=$(($stack + 100));
+stack=$(( $stack + 100));
 max=$(( $stack + $(( $stack / 2)) ));
 min=$(( $stack - $(( $stack / 2)) ));
+temp=$stack;
     while (( $stack < $max && $stack > $min))
     do
         gameStatus=$(( RANDOM % 2));
@@ -21,6 +26,15 @@ min=$(( $stack - $(( $stack / 2)) ));
         fi
     done
     everyDayGameStatus[$counter]=$stack;
+    if (( $stack > $temp));then
+        daysWon=$(( $daysWon + 1));
+        winingDays[$counter]=$(( $stack - $temp));
+    else
+        daysLost=$(( $daysLost + 1));
+        losingDays[$counter]=$(( $temp - $stack));
+    fi
 done
 echo ${everyDayGameStatus[@]};
+echo $daysWon;
+echo $daysLost;
 echo $stack;
